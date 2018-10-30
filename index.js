13b3d0f78a234c84bc51db22a3538b324147a0ba
冯建俊 <fengjj@knowbox.cn>
2018-10-30 16:54:53 +0800

    模拟数据

--- index.js
+++ index.js
@@ -51,8 +51,24 @@ var actionHandles = {
                 action:'5284',
                 data:{
                     timeOut:3,
-                    fatCatInfoList:[],
-                    catWeight:1800
+                    fatCatInfoList:[
+                        {
+                            "groupIndex":0,
+                            "weight":1800
+                        },
+                        {
+                            "groupIndex":1,
+                            "weight":1800
+                        },
+                        {
+                            "groupIndex":2,
+                            "weight":1800
+                        },
+                        {
+                            "groupIndex":3,
+                            "weight":1800
+                        }
+                    ]
                 }
             }
             ws.send(JSON.stringify(d1))
@@ -64,10 +80,101 @@ var actionHandles = {
                 data:{
                     timeOut:3,
                     finishGuide:0,
-                    configInfo:{},
-                    foodConfigList:[],
-                    teamList:[],
-                    score:10000
+                    configInfo:{
+                        diffucityFood:[
+                            {
+                                diffucity:2,
+                                score:100,
+                                food:[0,1,2]
+                            },
+                            {
+                                diffucity:3,
+                                score:200,
+                                food:[3,4,5]
+                            },
+                            {
+                                diffucity:4,
+                                score:300,
+                                food:[6,7,8]
+                            }
+                        ],
+                        catConf:[
+                            {
+                                mixWeight:100,
+                                maxWeight:300,
+                                catModle:0
+                            },
+                            {
+                                mixWeight:300,
+                                maxWeight:500,
+                                catModle:1
+                            },
+                            {
+                                mixWeight:500,
+                                maxWeight:99999,
+                                catModle:2
+                            }
+                            
+                        ],
+                        blowConf:[
+                            {
+                                "minCount": 0,
+                                "maxCount": 5,
+                                "bowl": 0,
+                                "catAnimal": 0,
+                                "eatFoodSpeed": 1.5
+                            },
+                            {
+                                "minCount": 6,
+                                "maxCount": 10,
+                                "bowl": 1,
+                                "catAnimal": 1,
+                                "eatFoodSpeed": 1
+                            },
+                            {
+                                "minCount": 11,
+                                "bowl": 2,
+                                "catAnimal": 1,
+                                "eatFoodSpeed": 1
+                            }
+
+                        ],
+                    },
+                    teamList:[
+                        {
+                            "userID": 'abc1',
+                            "userName": "123",
+                            "score":1231
+                        },
+                        {
+                            "userID": 'abc2',
+                            "userName": "123",
+                            "score":1231
+                        },
+                        {
+                            "userID": 'abc3',
+                            "userName": "123",
+                            "score":1231
+                        }
+                    ],
+                    fatCatInfoList:[
+                        {
+                            "groupIndex":0,
+                            "weight":1800
+                        },
+                        {
+                            "groupIndex":1,
+                            "weight":1800
+                        },
+                        {
+                            "groupIndex":2,
+                            "weight":1800
+                        },
+                        {
+                            "groupIndex":3,
+                            "weight":1800
+                        }
+                    ]
                 }
             }
             ws.send(JSON.stringify(d2))
@@ -103,7 +210,7 @@ var actionHandles = {
     /***
      * 学生端答题 
      */
-    '1265':function(ws){
+    '1265':function(ws,msg){
         var d2 = {
             action:'5286',
             data:{
@@ -128,7 +235,7 @@ app.ws('/ws', function(ws, req) {
     var data = JSON.parse(msg)
     console.log(data)
     if(typeof actionHandles[data.action] == 'function'){
-        actionHandles[data.action](ws)
+        actionHandles[data.action](ws,msg)
     }
   });
 })
